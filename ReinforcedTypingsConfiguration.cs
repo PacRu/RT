a5662f95fa82eb9db06a4ade262bf2d638620fd7
PacRu <40112921+PacRu@users.noreply.github.com>
2020-09-06 16:14:43 +0300

    Add files via upload

---
+++ ReinforcedTypingsConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+using Reinforced.Typings.Fluent;
+
+using Web.Models;
+
+namespace Web
+{
+    public static class ReinforcedTypingsConfiguration
+    {
+        public static void Configure(ConfigurationBuilder builder)
+        {
+            var viewModelTypes = Assembly.GetExecutingAssembly().DefinedTypes
+                .Where(v => string.Equals(typeof(ErrorViewModel).Namespace, v.Namespace, StringComparison.Ordinal))
+                .Where(v => v.Name.EndsWith("ViewModel"))
+                .ToArray();
+            builder.ExportAsInterfaces(viewModelTypes, options =>
+            {
+                options.WithAllProperties();
+            });
+        }
+    }
+}
